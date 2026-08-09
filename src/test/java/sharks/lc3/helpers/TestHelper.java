5586d7fa19862fefdddd680a4f2a836032d52351
Leopold <leopold@sharkscode.com>
2026-08-09 15:14:56 +0300

    chore: remove vcs.xml from git index and update gitignore

--- src/test/java/sharks/lc3/helpers/TestHelper.java
+++ src/test/java/sharks/lc3/helpers/TestHelper.java
@@ -1,11 +1,12 @@
-package sharks.lc3.helpers;
+package sharks.lc3;
 
 public class TestHelper {
+
     public static void logTestStart(String testName) {
         System.out.println(">>> RUNNING TEST: " + testName);
     }
+
     public static String sanitizePassword(String raw) {
         return raw == null ? "" : raw.trim();
     }
 }
-
