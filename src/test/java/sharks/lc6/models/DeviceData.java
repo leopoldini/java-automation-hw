d4baf7fb461501a9b0534d745840a7cb48302de1
Leopold <leopold@sharkscode.com>
2026-09-03 19:43:44 +0300

    refactor: apply camelCase and JsonProperty annotations for LC6 models

--- src/test/java/sharks/lc6/models/DeviceData.java
+++ src/test/java/sharks/lc6/models/DeviceData.java
@@ -1,32 +1,56 @@
 package sharks.lc6.models;
 
-public class DeviceData {
-    private final String platform;
-    private final String device_id;
-    private final String device_model;
-    private final String browser_name;
-    private final String browser_version;
-    private final String os_version;
-    private final String user_agent;
-    private final String language;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-    public DeviceData() {
-        this.platform = "WEB";
-        this.device_id = "9b07a82a58d36c9435a40704a8381570";
-        this.device_model = "Web Windows Chrome 151";
-        this.browser_name = "Chrome";
-        this.browser_version = "151.0.0.0";
-        this.os_version = "10";
-        this.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36";
-        this.language = "uk";
+public class DeviceData {
+
+    @JsonProperty("platform")
+    private final String platform;
+
+    @JsonProperty("device_id")
+    private final String deviceId;
+
+    @JsonProperty("device_model")
+    private final String deviceModel;
+
+    @JsonProperty("browser_name")
+    private final String browserName;
+
+    @JsonProperty("browser_version")
+    private final String browserVersion;
+
+    @JsonProperty("os_version")
+    private final String osVersion;
+
+    @JsonProperty("user_agent")
+    private final String userAgent;
+        public DeviceData() {
+        this.platform = "web";
+        this.deviceId = "unique_device_id";
+        this.deviceModel = "desktop";
+        this.browserName = "Chrome";
+        this.browserVersion = "120.0";
+        this.osVersion = "Windows 11";
+        this.userAgent = "Mozilla/5.0";
+    }
+
+    public DeviceData(String platform, String deviceId, String deviceModel,
+                      String browserName, String browserVersion,
+                      String osVersion, String userAgent) {
+        this.platform = platform;
+        this.deviceId = deviceId;
+        this.deviceModel = deviceModel;
+        this.browserName = browserName;
+        this.browserVersion = browserVersion;
+        this.osVersion = osVersion;
+        this.userAgent = userAgent;
     }
 
     public String getPlatform() { return platform; }
-    public String getDevice_id() { return device_id; }
-    public String getDevice_model() { return device_model; }
-    public String getBrowser_name() { return browser_name; }
-    public String getBrowser_version() { return browser_version; }
-    public String getOs_version() { return os_version; }
-    public String getUser_agent() { return user_agent; }
-    public String getLanguage() { return language; }
+    public String getDeviceId() { return deviceId; }
+    public String getDeviceModel() { return deviceModel; }
+    public String getBrowserName() { return browserName; }
+    public String getBrowserVersion() { return browserVersion; }
+    public String getOsVersion() { return osVersion; }
+    public String getUserAgent() { return userAgent; }
 }
--- src/test/java/sharks/lc6/models/RegisterRequest.java
+++ src/test/java/sharks/lc6/models/RegisterRequest.java
@@ -1,11 +1,25 @@
 package sharks.lc6.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class RegisterRequest {
+
+    @JsonProperty("type")
     private final String type;
+
+    @JsonProperty("email")
     private final String email;
+
+    @JsonProperty("password")
     private final String password;
+
+    @JsonProperty("promokey")
     private final String promokey;
-    private final int is_accept;
+
+    @JsonProperty("is_accept")
+    private final int isAccept;
+
+    @JsonProperty("device")
     private final DeviceData device;
 
     public RegisterRequest(UserData user) {
@@ -13,14 +27,14 @@ public class RegisterRequest {
         this.email = user.getEmail();
         this.password = user.getPassword();
         this.promokey = "";
-        this.is_accept = 1;
-        this.device = new DeviceData();
+        this.isAccept = 1;
+        this.device = new DeviceData(); // Ініціалізація поля device
     }
 
     public String getType() { return type; }
     public String getEmail() { return email; }
     public String getPassword() { return password; }
     public String getPromokey() { return promokey; }
-    public int getIs_accept() { return is_accept; }
+    public int getIsAccept() { return isAccept; }
     public DeviceData getDevice() { return device; }
 }
